049336df111c2016f5e716e9fa968f86fd6ae86e
jparep <parepson@gmail.com>
2024-03-22 21:50:56 -0600

    Transformers created

--- src/house_price_prediction.py
+++ src/house_price_prediction.py
@@ -1,2 +1,12 @@
+# Import Packages
 import numpy as np
 import pandas as pd
+from sklearn.impute import IterativeImputer, SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, RobustScaler
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingRegressor
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+
